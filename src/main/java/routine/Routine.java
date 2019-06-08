76887965abe05410da2401158fd78e2e6d18dbf3
Daniel <dbgodfrey89@msn.com>
2019-06-08 08:49:13 -0600

    Add AM/PM to time

--- src/main/java/routine/Routine.java
+++ src/main/java/routine/Routine.java
@@ -9,17 +9,25 @@ public interface Routine {
     interface Time {
         String time();
 
-        final class Default implements Time {
+        Time plusMinutes(int minutes);
+
+        final class Standard implements Time {
+            public enum State {
+                AM, PM
+            }
+
             private final int mHour;
             private final int mMinute;
+            private final State mState;
 
-            public Default(final int hour, final int minute) {
+            public Standard(final int hour, final int minute, final State state) {
                 mHour = hour;
                 mMinute = minute;
+                mState = state;
             }
 
             public static Time from(final String string) {
-                final Pattern pattern = Pattern.compile("([01]?\\d):([0-5]\\d)");
+                final Pattern pattern = Pattern.compile("([01]?\\d):([0-5]\\d)\\s*(AM|PM)?");
                 final Matcher matcher = pattern.matcher(string);
                 final boolean found = matcher.find();
                 if (!found) {
@@ -29,21 +37,52 @@ public interface Routine {
                 final Integer hour = Integer.parseInt(hourString);
                 final String minuteString = matcher.group(2);
                 final Integer minute = Integer.parseInt(minuteString);
-                return new Time.Default(hour, minute);
+                final String stateString = matcher.group(3);
+                final State state;
+                if (stateString == null) {
+                    state = State.AM;
+                } else if (stateString.equals(State.AM.toString())) {
+                    state = State.AM;
+                } else if (stateString.equals(State.PM.toString())) {
+                    state = State.PM;
+                } else {
+                    throw new IllegalArgumentException("Could not match " + stateString + " to AM or PM");
+                }
+                return new Time.Standard(hour, minute, state);
             }
 
             @Override
             public String time() {
-                return String.format("%02d:%02d", mHour, mMinute);
+                return String.format("%02d:%02d %s", mHour, mMinute, mState.toString());
             }
 
             @Override
             public boolean equals(final Object obj) {
-                if (!(obj instanceof Default)) {
+                if (!(obj instanceof Standard)) {
                     return false;
                 }
-                final Default that = (Default) obj;
+                final Standard that = (Standard) obj;
                 return (mHour == that.mHour) && (mMinute == that.mMinute);
+            }
+
+            @Override
+            public Time plusMinutes(final int minutes) {
+                final int minuteDifference = mMinute + minutes;
+                final int hourDifference = minuteDifference / 60;
+                final int newMinute = minuteDifference % 60;
+                final int newHour = mHour + hourDifference;
+                final State state;
+                if ((newHour + hourDifference) > 12) {
+                    if (mState.equals(State.PM)) {
+                        throw new IllegalStateException(String.format(
+                                "%d minutes past %s crosses days, which is not currently supported", minutes, time()));
+                    } else {
+                        state = State.PM;
+                    }
+                } else {
+                    state = mState;
+                }
+                return new Time.Standard(newHour, newMinute, state);
             }
         }
     }
--- src/test/java/routine/TestTime.java
+++ src/test/java/routine/TestTime.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import routine.Routine.Time;
+import routine.Routine.Time.Standard.State;
 
 public class TestTime {
 
@@ -14,14 +15,14 @@ public class TestTime {
     final class TestDefault {
         @Test
         public void from() {
-            final Time time = Time.Default.from("8:27");
-            assertEquals(new Time.Default(8, 27), time);
+            final Time time = Time.Standard.from("8:27 AM");
+            assertEquals(new Time.Standard(8, 27, State.AM), time);
         }
 
         @Test
         public void fromNotFound() {
             try {
-                Time.Default.from("1008.87354");
+                Time.Standard.from("1008.87354");
                 fail();
             } catch (final IllegalArgumentException e) {
 
@@ -30,8 +31,32 @@ public class TestTime {
 
         @Test
         public void time() {
-            final Time time = Time.Default.from("8:27");
-            assertEquals("08:27", time.time());
+            final Time time = Time.Standard.from("8:27 AM");
+            assertEquals("08:27 AM", time.time());
+        }
+
+        @Test
+        public void plusMinutes() {
+            final Time time = Time.Standard.from("8:27 AM");
+            final Time newTime = time.plusMinutes(3);
+            assertEquals(new Time.Standard(8, 30, State.AM), newTime);
+        }
+
+        @Test
+        public void plusMinutesCrossHour() {
+            final Time time = Time.Standard.from("8:59 AM");
+            final Time newTime = time.plusMinutes(2);
+            assertEquals(new Time.Standard(9, 1, State.AM), newTime);
+        }
+
+        @Test
+        public void plusMinutesCrossDay() {
+            try {
+                final Time time = Time.Standard.from("11:59 PM");
+                time.plusMinutes(2);
+                fail();
+            } catch (final IllegalStateException e) {
+            }
         }
     }
 }
